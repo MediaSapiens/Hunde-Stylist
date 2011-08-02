258541f44d1bf011e39813c33fa924f1f13ce773
Pavel Kholodyr <beriagts@gmail.com>
2011-08-02 17:56:10 +0300

    delete table from main page. make script for drop-down cart in top. edit account page 4/

--- js/myscript.js
+++ js/myscript.js
@@ -1,26 +1,14 @@
  $(document).ready(function(){
-              var helina = $('.fullorder').outerHeight();
-                          var b =helina-25;
-               $('.fullorder').css('marginTop',-b);
-               $('#cart').click(function(){  
-                 if($(this).hasClass('clicked')){                 
-                        $('.fullorder').animate({marginTop:-b},1500);
-                        $('#cart a').css('backgroundPosition','0 0');
-                        $('#cart').removeClass('clicked');                                          
-                 }
-                 else{   
-                       $('.fullorder').animate({marginTop:0},1500);
-                       $('#cart a').css('backgroundPosition','0 -258px');
-                       $('#cart').addClass('clicked');
-                   }
-                   return false;
-                 });
-                  $("tr a.cls").click(function(){
-                        $(this).parents("tr").remove();
+                
+	$("#cart").click(function(){
+		$(".fullorder").slideToggle('1400');
+		$(this).toggleClass("active"); return false;
+	});
+    $(".amount a.cls").click(function(){
+                        $(this).parents(".listprod").remove();
                           
                         return false;
                     });
-                 
 
                 $('.info').click(function(){
                  
