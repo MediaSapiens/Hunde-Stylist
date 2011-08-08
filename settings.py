2b51e08050d32dfa066fcc60530cbda872f6953b
Vadim Chernysh <chernysh.vadim@gmail.com>
2011-08-08 17:55:27 +0300

    fixes #8 -- Project skeleton done

---
+++ settings.py
@@ -0,0 +1,122 @@
+# -*- coding: utf8 -*-
+from os import path
+import os
+import sys
+import re
+
+_ = lambda s: s
+gettext = lambda s: s
+
+PROJECT_ROOT = path.dirname(path.abspath(__file__))
+
+sys.path.insert(0,path.join(PROJECT_ROOT, 'apps'))
+sys.path.insert(0,path.join(PROJECT_ROOT, 'libs'))
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Vadim Chernysh', 'chernysh.vadim@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': path.join(PROJECT_ROOT, 'hs.sqlite3.db'),
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+TIME_ZONE = 'America/Chicago'
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+
+MEDIA_ROOT = path.join(PROJECT_ROOT, 'static/media')
+MEDIA_URL = '/static/media/'
+STATIC_ROOT = path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+STATICFILES_DIRS = (
+    # path.join(PROJECT_ROOT, 'static')
+)
+
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '2%068-=k4jshp)!50hi)t1dfv(@0!dq6@_l40$ha(qf)1@oqq4'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'urls'
+
+TEMPLATE_DIRS = (
+    path.join(PROJECT_ROOT, 'templates'),
+)
+
+FIXTURE_DIRS = (
+    path.join(PROJECT_ROOT, 'fixtures'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'satchless',
+    
+    'shop',
+)
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
